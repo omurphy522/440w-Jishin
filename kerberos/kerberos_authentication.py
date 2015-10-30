89594af9b1b7f511886ee650f74022839e350930
Nabil Azam <nazam9519@gmail.com>
2015-10-29 21:31:40 -0400

    Kerberos can now check for specific users

--- kerberos/kerberos_authentication.py
+++ kerberos/kerberos_authentication.py
@@ -1,41 +1,35 @@
 #!/usr/bin/python
 import subprocess
 import os
-
 import sys
 sys.path.append('..')
-
 import ConstantValues.Constants
 
 class kerberosHandler:
 
     def has_kerberos_ticket(self, username):
-        if subprocess.call(['klist', '-s']) == 0:
-            print('you\'re authenticated')
-            return True
+	
+	#checks to see if tickets are in the KDC
+	if subprocess.call(['klist','-s']) == 0:
+        
+	#checks to see if specified username is active in klist
+                list_tick = subprocess.Popen(('klist | grep ' + username), stdout=subprocess.PIPE, shell = True)
+        
+	#stores the ouput of list_tick into variable 'out'
+                out, err = list_tick.communicate()
+        
+	#this basically says if there's no output from klist, there's no ticket and the user must authenticate
+        
+		if out == '':
+                	subprocess.call(['kinit', username])
+        	else:
+                	"""congrats you've been authenticated"""
+                	print('you\'re all good to go')
+	
+	else: #if there are no tickets in the kerberos cache...
+                print('no users currently have tickets')
+                username = raw_input('enter username: ')
+                subprocess.call(['kinit',username])
+        
 
-        else:
-            raw_input('There is no recorded ticket, please enter your password \n')
-            authenticate = subprocess.call(['kinit', username])
-            # since kerberos doesnt return anything we can have it return this value
-            return str(ConstantValues.Constants.ConstantValues.AUTHENTICATED.value)
-    # has_kerberos_ticket()
-
-# def create_token(usrnam):
-# 	key = 'secret'
-# 	payload = {'username':usrnam ,'authentication':'auth'}
-# 	token = Token.encode(payload, key, 'HS256')
-# 	print token
-# 	decode = Token.decode(token, 'secret', 'HS256')
-# 	print decode
-#         #if authenticate == 0:
-#          #   while(False):
-#           #     authenticate
-#            #    if authenticate == 0:
-#             #       return True
-#              #      has_kerberos_ticket()
-#               # else:
-#                #    print('not authenticated')
-#                 #   return False
-# has_kerberos_ticket()
-# create_token(entername)
+        return str(ConstantValues.Constants.ConstantValues.AUTHENTICATED.value)
