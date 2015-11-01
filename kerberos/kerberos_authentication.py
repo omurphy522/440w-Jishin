d7ff974c8031d7ff3d7d799af87e4bf34aa8233c
Nabil Azam <nazam9519@gmail.com>
2015-11-01 15:54:15 -0500

    Added Exception handling

--- kerberos/kerberos_authentication.py
+++ kerberos/kerberos_authentication.py
@@ -1,35 +1,41 @@
 #!/usr/bin/python
-import subprocess
-import os
-import sys
-sys.path.append('..')
-import ConstantValues.Constants
+try:
 
-class kerberosHandler:
+	import subprocess
+	import os
+	import sys
+	sys.path.append('..')
+	import ConstantValues.Constants
 
-    def has_kerberos_ticket(self, username):
+	class kerberosHandler:
+
+    	   def has_kerberos_ticket(self, username):
 	
-	#checks to see if tickets are in the KDC
-	if subprocess.call(['klist','-s']) == 0:
+		#checks to see if tickets are in the KDC
+		if subprocess.call(['klist','-s']) == 0:
         
-	#checks to see if specified username is active in klist
-                list_tick = subprocess.Popen(('klist | grep ' + username), stdout=subprocess.PIPE, shell = True)
+		#checks to see if specified username is active in klist
+                	list_tick = subprocess.Popen(('klist | grep ' + username), stdout=subprocess.PIPE, shell = True)
         
-	#stores the ouput of list_tick into variable 'out'
-                out, err = list_tick.communicate()
+		#stores the ouput of list_tick into variable 'out'
+                	out, err = list_tick.communicate()
         
-	#this basically says if there's no output from klist, there's no ticket and the user must authenticate
+		#this basically says if there's no output from klist, there's no ticket and the user must authenticate
         
-		if out == '':
-                	subprocess.call(['kinit', username])
-        	else:
-                	"""congrats you've been authenticated"""
-                	print('you\'re all good to go')
+			if out == '':
+                		subprocess.call(['kinit', username])
+        		else:
+                		"""congrats you've been authenticated"""
+                		print('you\'re all good to go')
 	
-	else: #if there are no tickets in the kerberos cache...
-                print('no users currently have tickets')
-                username = raw_input('enter username: ')
-                subprocess.call(['kinit',username])
+		else: #if there are no tickets in the kerberos cache...
+                	print('no users currently have tickets')
+                	username = raw_input('enter username: ')
+                	subprocess.call(['kinit',username])
         
 
-        return str(ConstantValues.Constants.ConstantValues.AUTHENTICATED.value)
+        	return str(ConstantValues.Constants.ConstantValues.AUTHENTICATED.value)
+except ImportError as e:
+	print("Broken Impot Statement")
+except IOError as (errno,strerror):
+	print("IOError")
