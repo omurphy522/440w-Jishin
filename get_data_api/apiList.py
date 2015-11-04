b3bb81a5bb1ec9c59cd927c09cd6b871e76efafd
Brian Gracin <gracinbd@gmail.com>
2015-11-04 15:40:26 -0500

    add trys and comments

--- get_data_api/apiList.py
+++ get_data_api/apiList.py
@@ -1,5 +1,14 @@
-def get_api_urls():
-    api_urls = [
+#Name: apiCollectionFreshPull
+#Author: Brian Gracin
+#Course: IST 440W
+#Professor: Senior Lecturer Joseph Oakes
+#Created: 10/19/2015
+#Modified: 11/2/2015
+#List compiled by Owen Murphy
+
+def getApiUrls():
+    #List holding collection name and api url for parsing
+    apiUrls = [
     {'name':'us_all_w', 'url':'http://api.eia.gov/series/?api_key=D7448759E3B234C54606C2E410368612&series_id=PET.EMM_EPM0_PTE_NUS_DPG.W&out=xml'},
     {'name':'east_coast_all_w', 'url':'http://api.eia.gov/series/?api_key=D7448759E3B234C54606C2E410368612&series_id=PET.EMM_EPM0_PTE_R10_DPG.W&out=xml'},
     {'name':'new_england_all_w', 'url':'http://api.eia.gov/series/?api_key=D7448759E3B234C54606C2E410368612&series_id=PET.EMM_EPM0_PTE_R1X_DPG.W&out=xml'},
@@ -10,5 +19,6 @@ def get_api_urls():
     {'name':'rock_mountains_all_w', 'url':'http://api.eia.gov/series/?api_key=D7448759E3B234C54606C2E410368612&series_id=PET.EMM_EPM0_PTE_R40_DPG.W&out=xml'},
     {'name':'west_coast_all_w', 'url':'http://api.eia.gov/series/?api_key=D7448759E3B234C54606C2E410368612&series_id=PET.EMM_EPM0_PTE_R50_DPG.W&out=xml'},
     {'name':'west_coast_no_cali_all_w', 'url':'http://api.eia.gov/series/?api_key=D7448759E3B234C54606C2E410368612&series_id=PET.EMM_EPM0_PTE_R5XCA_DPG.W&out=xml'},
-    {'name':'us_all_a', 'url':'http://api.eia.gov/series/?api_key=D7448759E3B234C54606C2E410368612&series_id=PET.EMM_EPM0_PTE_NUS_DPG.A&out=xml'}]
-    return api_urls
+    {'name':'us_all_a', 'url':'http://api.eia.gov/series/?api_key=D7448759E3B234C54606C2E410368612&series_id=PET.EMM_EPM0_PTE_NUS_DPG.A&out=xml'},
+    {'name':'us_all_m', 'url':'http://api.eia.gov/series/?api_key=D7448759E3B234C54606C2E410368612&series_id=PET.EMM_EPM0_PTE_NUS_DP&out=xml'}]
+    return apiUrls
