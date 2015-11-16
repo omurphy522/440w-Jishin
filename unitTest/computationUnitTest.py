3a3b30d1d9dc271753d5f680dc0706929c0aca7f
Brian Gracin <gracinbd@gmail.com>
2015-11-16 16:12:07 -0500

    unified api parsing into a single class Refactored computation and api classes

---
+++ unitTest/computationUnitTest.py
@@ -0,0 +1,8 @@
+# Name: computationUnitTest
+# Author(s): Brian Gracin
+# Course: IST 440W
+# Professor: Senior Lecturer Joseph Oakes
+# Created: 11/16/2015
+# Modified: 11/18/2015
+
+import unittest
