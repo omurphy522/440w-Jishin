6789aa7c1d11c485735d25358276ef15e131fb6b
Owen Murphy <ocm5020@Team1.(none)>
2015-11-24 08:43:14 -0500

    deleted

--- logging_owen/testOwenLogging.py
+++
@@ -1,9 +0,0 @@
-__author__ = 'owen'
-
-import handler_logging
-
-handler_logging.logger.error('error message')
-handler_logging.logger.critical('critical message')
-handler_logging.logger.debug('debug message')
-handler_logging.logger.info('info message')
-handler_logging.logger.warning('warning message')
