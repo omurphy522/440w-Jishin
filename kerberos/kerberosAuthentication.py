e7a116adbd0f007525a7c2bd7883c2f3474addb4
Owen Murphy <ocm5020@Team1.(none)>
2015-12-07 22:46:17 -0500

    modified jishin submit and kerbAuth for finilization

--- kerberos/kerberosAuthentication.py
+++ kerberos/kerberosAuthentication.py
@@ -14,16 +14,16 @@ try:
 
         def has_kerberos_ticket(self, username, password):
 
-            subprocess.Popen(['kdestroy'])
+            subprocess.call(['kdestroy'])
             jishinLogging.logger.info('no tickets')
             user = subprocess.Popen(['echo', password], stdout=subprocess.PIPE)
             userpass = subprocess.Popen(['kinit', username], stdin=user.stdout, stderr=subprocess.STDOUT)
             storepass, err = userpass.communicate()
 
-            # list_tick = subprocess.Popen(('klist | grep ' + username), stdout=subprocess.PIPE, shell=True)
-            list_tick = subprocess.Popen(['klist'], stdout=subprocess.PIPE)
-            list_tick2 = subprocess.Popen(['grep', username], stdin=list_tick.stdout, stderr=subprocess.STDOUT)
-            out, err = list_tick2.communicate()
+            list_tick = subprocess.Popen(('klist | grep ' + username), stdout=subprocess.PIPE, shell=True)
+           # list_tick = subprocess.Popen(['klist'], stdout=subprocess.PIPE)
+           # list_tick2 = subprocess.Popen(['grep', username], stdin=list_tick.stdout, stderr=subprocess.STDOUT)
+            out, err = list_tick.communicate()
             if out != '':
                 return True
             else:
