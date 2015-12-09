dcd6f93f8a549a9579f6a2d6176233bd4cbe861d
omurphy522 <ocm522@gmail.com>
2015-12-09 15:29:00 -0500

    Update kerberosAuthentication.py

--- kerberos/kerberosAuthentication.py
+++ kerberos/kerberosAuthentication.py
@@ -16,17 +16,10 @@ try:
 
             subprocess.call(['kdestroy'])       
             user = subprocess.Popen(['echo', password], stdout=subprocess.PIPE)
- #           print "before userpass"
 	    userpass = subprocess.Popen(['kinit', username], stdin=user.stdout, stderr=subprocess.STDOUT)
-#            print "after userpass"
 	    storepass, err = userpass.communicate()
-#	    print "before listtick"
             list_tick = subprocess.Popen(('klist | grep ' + username), stdout=subprocess.PIPE, shell=True)
-            # list_tick = subprocess.Popen(['klist'], stdout=subprocess.PIPE)
-            # list_tick2 = subprocess.Popen(['grep', username], stdin=list_tick.stdout, stderr=subprocess.STDOUT)
-#            print "Before out"
 	    out, err = list_tick.communicate()
-#	    print "be4 if"
             if out != '':
 		jishinLogging.logger.info('%s Logged In' %username)
                 return True
